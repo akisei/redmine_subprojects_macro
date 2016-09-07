d9777fa6bec398ac4f000fd5b75527020f98d276
akisei <seikun1234@gmail.com>
2016-09-07 18:11:20 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_subprojects_macro do
+  name 'Redmine Subprojects Macro plugin'
+  author 'akisei'
+  description 'Add a macro to display sub-projects plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/akisei/redmine_subprojects_macro'
+  author_url 'https://github.com/akisei'
+end
